014bc49c089bd9dee912ae0886cf39b42204e27c
Frank Wei <frank@frankwei.xyz>
2019-01-17 16:13:30 +0800

    feat: chunk renaming

--- src/page/index.js
+++ src/page/index.js
@@ -4,9 +4,9 @@
 
 import loadable from '@loadable/component'
 
-const AboutUs = loadable(() => import('./About'))
-const Keygen = loadable(() => import('./Keygen'))
-const Encrypt = loadable(() => import('./encrypt'))
-const Decrypt = loadable(() => import('./decrypt'))
+const AboutUs = loadable(() => import(/* webpackChunkName: "about" */'./About'))
+const Keygen = loadable(() => import(/* webpackChunkName: "keygen" */'./Keygen'))
+const Encrypt = loadable(() => import(/* webpackChunkName: "encrypt" */'./encrypt'))
+const Decrypt = loadable(() => import(/* webpackChunkName: "decrypt" */'./decrypt'))
 
 export { Encrypt, Decrypt, AboutUs, Keygen }
