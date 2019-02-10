6c557988ffabde35d2c8fdb7818986a4f49c92d6
Frank Wei <frank@frankwei.xyz>
2019-02-10 11:23:15 +0800

    more declarative

--- src/page/KeyManagement.jsx
+++ src/page/KeyManagement.jsx
@@ -3,21 +3,22 @@ import openpgp, { key } from "openpgp";
 import { withStore } from "../store";
 import CopyableTextarea from "../components/CopyableTextarea";
 
-function MyKeyPair({ store }) {
-    return (
-        <div className="mykey columns">
-            <div className="control column">
-                <h2 className="title">你的公钥 Public Key</h2>
-                <div className="notification is-info">请把公钥分享给朋友，让他通过公钥给你发悄悄话</div>
-                <CopyableTextarea name="publicKey" text={store.get('publicKey').armor()}></CopyableTextarea>
-            </div>
-            <div className="control column">
-                <h2 className="title">你的私钥 Private Key</h2>
-                <div className="notification is-danger">请不要分享私钥给任何人<br />你的私钥和密码组合一起，可以解开只属于你的秘密！</div>
-                <CopyableTextarea name="privateKey" text={store.get('privateKey').armor()}></CopyableTextarea>
-            </div>
+function MyKeyPair({ store, isKeyExist }) {
+    if (!isKeyExist) {
+        return false // Not rendering if key pair is not exist
+    }
+    return <div className="mykey columns">
+        <div className="control column">
+            <h2 className="title">你的公钥 Public Key</h2>
+            <div className="notification is-info">请把公钥分享给朋友，让他通过公钥给你发悄悄话</div>
+            <CopyableTextarea name="publicKey" text={store.get('publicKey').armor() || ''}></CopyableTextarea>
         </div>
-    )
+        <div className="control column">
+            <h2 className="title">你的私钥 Private Key</h2>
+            <div className="notification is-danger">请不要分享私钥给任何人<br />你的私钥和密码组合一起，可以解开只属于你的秘密！</div>
+            <CopyableTextarea name="privateKey" text={store.get('privateKey').armor() || ''}></CopyableTextarea>
+        </div>
+    </div>
 }
 
 function KeyManagement(props) {
@@ -47,15 +48,12 @@ function KeyManagement(props) {
     return (
         <div className="key-management">
             <h1 className="title">密钥管理</h1>
-            {isKeyExist
-                ?
-                <div className="notification is-danger key-exist">
-                    你已经生成了密钥对，需要更换密钥吗？
-                </div>
-                :
-                <div className="notification is-info key-not-exist">
+            {isKeyExist ? (<div className="notification is-danger key-exist">
+                你已经生成了密钥对，需要更换密钥吗？
+                </div>)
+                : (<div className="notification is-info key-not-exist">
                     你还没有密钥对，填写下方信息即可生成（仅供验证密文用，无需真实信息）
-                </div>
+                </div>)
             }
             <form>
                 <input className="input" type="text" placeholder="名字或昵称，用于辨识密钥身份"
@@ -69,7 +67,7 @@ function KeyManagement(props) {
                 <button type="button" className="button primary"
                     onClick={() => keyGenerate()} >生成</button>
             </form>
-            {isKeyExist && MyKeyPair(props)}
+            <MyKeyPair isKeyExist={isKeyExist} {...props} />
         </div>
     )
 }
